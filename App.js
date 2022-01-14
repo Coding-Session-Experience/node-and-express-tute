b192d2c086c3fa1ff6d5e88be5304c8b18abccf4
Dulanga Nimesha <idulanganimesha@gmail.com>
2022-01-14 21:11:58 +0530

    Add http.js and package,json plugins

---
+++ App.js
@@ -0,0 +1,12 @@
+//npm --version
+
+//local dependency
+// npm i <packageName>
+//global dependency
+// npm i -g <packageName>
+
+// package.json - manifest file (stores important about project/package>
+// manual approach (create package.json in the root, create properties)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
