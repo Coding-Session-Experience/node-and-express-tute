af1684968b21c05ce7f2073bdf42e808800f5ac5
Dulanga Nimesha <idulanganimesha@gmail.com>
2022-01-15 11:18:48 +0530

    add event loop examples

--- App.js
+++ App.js
@@ -10,9 +10,27 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
 
-const _ = require('lodash')
+// const _ = require('lodash')
+//
+// const items = [1, [2, [3, [4]]]]
+// const newItems = _.flattenDeep(items)
+// console.log(newItems);
+// console.log('hello people')
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems);
 
+
+// started operating system process
+// console.log('first');
+// setTimeout(() => {
+//     console.log('second');
+// }, 0)
+// console.log('third');
+//completed and exited operating system process
+
+setInterval(() => {
+    console.log('hello world');
+}, 2000)
+console.log('i will run first');
+// process stays alive unless
+// Kill Process CTRL + C
+// unexpected error
