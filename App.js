dedc8961ca7cc8f57e97980c4da159ce3e1ee55e
Dulanga Nimesha <idulanganimesha@gmail.com>
2022-01-17 22:33:17 +0530

    Add update

--- App.js
+++ App.js
@@ -11,26 +11,24 @@
 // npm init -y (everything default)
 
 // const _ = require('lodash')
-//
+
 // const items = [1, [2, [3, [4]]]]
 // const newItems = _.flattenDeep(items)
 // console.log(newItems);
 // console.log('hello people')
 
 
+const {readFile} = require('fs')
 
-// started operating system process
-// console.log('first');
-// setTimeout(() => {
-//     console.log('second');
-// }, 0)
-// console.log('third');
-//completed and exited operating system process
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                console.log(data);
+            }
+        })
+    })
+}
 
-setInterval(() => {
-    console.log('hello world');
-}, 2000)
-console.log('i will run first');
-// process stays alive unless
-// Kill Process CTRL + C
-// unexpected error
